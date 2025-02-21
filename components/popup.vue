0cbce67a685a90898f8ae0995fa6ac1ef5fb334f
Codaea <dakotajayroth@gmail.com>
2025-02-20 16:28:03 -0800

    added basic popup functionality

---
+++ components/popup.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+ <!-- popup window -->
+        <div class="absolute bg-blue-500" :style="{top: y + 'px', left: x + 'px', width: props.width + 'px', height: props.height + 'px'}" @mousedown="startDrag">
+            <p>POPUP WINDOW</p>
+        </div>   
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    x: {
+        type: Number,
+        required: true
+    },
+    y: {
+        type: Number,
+        required: true
+    },
+    width: Number,
+    height: Number,
+})
+
+// x and y from props are only used to initialize the ref values
+const x = ref(props.x);
+const y = ref(props.y);
+// offset allows to drag the popup window from the current mouse position instead of teleporting window to top left corner
+const offSetX = ref(0);
+const offSetY = ref(0);
+
+const isDragging = ref(false);
+
+function startDrag(e: MouseEvent) {
+    isDragging.value = true;
+    offSetX.value = e.clientX - x.value;
+    offSetY.value = e.clientY - y.value;
+    window.addEventListener('mousemove', dragEl);
+    window.addEventListener('mouseup', stopDrag);
+}
+
+function stopDrag() {
+    isDragging.value = false;
+    window.removeEventListener('mousemove', dragEl);
+    window.removeEventListener('mouseup', stopDrag);
+}
+
+function dragEl(e: MouseEvent) {
+    if (isDragging.value) {
+        x.value = e.clientX - offSetX.value;
+        y.value = e.clientY - offSetY.value;
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class>
-
+    <!-- Screen + background -->
+    <div class="w-screen h-screen bg-gray-100">
+        <Popup :x="0" :y="100" :width="1000" :height="500" />
     </div>
 </template>
 <script setup lang="ts">
