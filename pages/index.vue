475880ded2ef45fa12fa8d0e64b711cf41939f82
Codaea <dakotajayroth@gmail.com>
2025-02-20 16:43:53 -0800

    added dynamic popup generation with random positioning and sizing

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,36 @@
 <template>
     <!-- Screen + background -->
     <div class="w-screen h-screen bg-gray-100">
-        <Popup :x="0" :y="100" :width="1000" :height="500" />
+        <div v-for="popup in popups">
+            <Popup :x="popup.x" :y="popup.y"  :width="popup.width" :height="popup.height"/>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
+interface Popup {
+    x: number 
+    y: number
+    width: number
+    height: number
+}
+const popups = ref<Popup[]>([])
+const screenX = 1920 // fetch dynamicly
+const screenY = 1080
+
+for (let i = 0; i < 10; i++) {
+    const popup: Popup = {
+        width: randIntInclusive(100, 500),
+        height: randIntInclusive(100, 500),
+        x: randIntInclusive(0, screenX),
+        y: randIntInclusive(0, screenY)
+    }
+    popups.value.push(popup);
+}
+
+
+function randIntInclusive(min: number, max:number) : number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 </script>
